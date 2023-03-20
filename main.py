b7da0eccc951d7ce74bfc1e3b562dc002c10da64
SoongE <oh1.seungmin@gmail.com>
2023-03-20 22:35:53 +0900

    Update main file

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import hydra
 import torch
 from omegaconf import DictConfig
-from timm import create_model
 from timm.utils import CheckpointSaver
 
 from src.data import get_dataloader
 from src.fit import Fit
 from src.initial_setting import init_seed, init_distributed, init_logger, cuda_setting
-from src.utils import model_tune, create_scheduler_v2, create_criterion, NativeScalerWithGradUpdate, \
-    logging_benchmark_result_to_wandb, benchmark_model
-from src.models import *
+from src.utils import model_tune, logging_benchmark_result_to_wandb, benchmark_model
+from src.utils.factory import ObjectFactory
 
 
 @hydra.main(config_path="configs", config_name="config", version_base="1.3")
@@ -22,14 +20,14 @@ def main(cfg: DictConfig) -> None:
 
     init_logger(cfg)
 
-    model = create_model(**cfg.model, num_classes=cfg.dataset.num_classes)
-    criterions = create_criterion(cfg, device)
-    scaler = NativeScalerWithGradUpdate()
-
-    model, optimizer, model_ema, resume_epochs = model_tune(model, scaler, device, cfg)
-
-    scheduler, num_epochs, start_epoch = create_scheduler_v2(cfg, optimizer, resume_epochs)
     loaders = get_dataloader(cfg)
+    factory = ObjectFactory(cfg)
+
+    model = factory.create_model()
+    optimizer, scheduler = factory.create_optimizer_and_scheduler(model, len(loaders[0]))
+    criterion, scaler = factory.create_criterion_scaler()
+
+    model, model_ema = model_tune(model, cfg)
 
     saver = CheckpointSaver(model=model, optimizer=optimizer, args=cfg, model_ema=model_ema, amp_scaler=scaler,
                             max_history=cfg.train.save_max_history)
@@ -38,8 +36,8 @@ def main(cfg: DictConfig) -> None:
         benchmark_result = benchmark_model(cfg.benchmark, model)
         logging_benchmark_result_to_wandb(benchmark_result, cfg.name)
 
-    fit = Fit(cfg, scaler, device, start_epoch, num_epochs, model, criterions, optimizer, model_ema, scheduler, saver,
-              loaders)
+    cfg = factory.cfg
+    fit = Fit(cfg, scaler, device, cfg.train.epochs, model, criterion, optimizer, model_ema, scheduler, saver, loaders)
 
     fit()
 
