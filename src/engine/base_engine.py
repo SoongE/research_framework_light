2f55cabef67c1d8c5d6e850130425c8e47abb656
SoongE <oh1.seungmin@gmail.com>
2023-07-04 19:43:10 +0900

    Rename engine from fit

--- src/engine/base_engine.py
+++ src/engine/base_engine.py
@@ -2,17 +2,19 @@ import logging
 
 import torch
 import torchmetrics
+from timm.models import model_parameters
 from timm.utils import dispatch_clip_grad, distribute_bn, update_summary
 from torch.cuda.amp import autocast
 from torchmetrics import MeanMetric
 
 
-class Fit:
+class Engine:
     def __init__(self, cfg, scaler, device, epochs, model, criterion, optimizer, model_ema, scheduler, saver,
                  loader=None):
         self.device = device
         self.local_rank = cfg.local_rank
         self.world_size = cfg.world_size
+        self._master_node = cfg.is_master
 
         self.distributed = cfg.distributed
         self.dist_bn = cfg.train.dist_bn
@@ -22,13 +24,13 @@ class Fit:
         self.double_valid = cfg.train.double_valid
         self.wandb = cfg.wandb
         self.start_epoch, self.num_epochs = epochs
-        self.logging_interval = 100
+        self.logging_interval = 50
         self.num_classes = cfg.dataset.num_classes
         self.tm = cfg.train.target_metric
         self.eval_metrics = cfg.train.eval_metrics
 
         self.model = model
-        if isinstance(criterion, list):
+        if isinstance(criterion, (list, tuple)):
             self.train_criterion = criterion[0]
             self.val_criterion = criterion[1]
         else:
@@ -65,14 +67,15 @@ class Fit:
                 eval_metrics = self.validate(epoch, ema=False)
 
             # save proper checkpoint with eval metric
-            if self._master_node():
+            if self._master_node:
                 torch.cuda.synchronize()
                 best_metric, best_epoch = self.saver.save_checkpoint(epoch, metric=eval_metrics[self.tm])
                 eval_metrics.update({f'Best_{self.tm}': best_metric})
                 update_summary(epoch, train_metrics, eval_metrics, 'summary.csv', log_wandb=self.wandb,
                                write_header=(epoch == 0))
 
-        if self._master_node():
+            self.scheduler.step(epoch + 1, eval_metrics[self.tm])
+        if self._master_node:
             logging.info(f'*** Best {self.tm}: {best_metric} {best_epoch} ***')
 
     def iterate(self, model, data, criterion):
@@ -87,23 +90,42 @@ class Fit:
 
     def train(self, epoch):
         self._reset_metric()
+        second_order = hasattr(self.optimizer, 'is_second_order') and self.optimizer.is_second_order
+
         total = len(self.train_loader)
+        accum_steps = self.grad_accumulation
+        num_updates = epoch * (updates_per_epoch := (total + accum_steps - 1) // accum_steps)
+        last_batch_idx_to_accum = total - (last_accum_steps := total % accum_steps)
 
         self.model.train()
         self.optimizer.zero_grad()
         for i, data in enumerate(self.train_loader):
-            loss, prob, target = self.iterate(self.model, data, self.train_criterion)
-            loss /= self.grad_accumulation
+            update_grad = i == total - 1 or (i + 1) % accum_steps == 0
+            update_idx = i // accum_steps
+            accum_steps = last_accum_steps if i >= last_batch_idx_to_accum else accum_steps
 
-            self._backward(loss, (i + 1) % self.grad_accumulation == 0)
+            loss, prob, target = self.iterate(self.model, data, self.train_criterion)
+            loss = loss / accum_steps if accum_steps > 1 else loss
+            self._backward(loss, update_grad, second_order)
+
             self.losses.update(loss)
 
-            _computed_loss = self.losses.compute()
-            if self._master_node() and i % self.logging_interval == 0:
-                logging.info(f'Train {epoch:>3}: [{i:>4}/{total}]  loss:{_computed_loss:.6f}')
+            if not update_grad:
+                continue
 
-            torch.cuda.synchronize()
-            self.scheduler.step()
+            num_updates += 1
+            mean_loss = self.losses.compute()
+            if update_idx % self.logging_interval == 0 or i == total - 1:
+                lrl = [param_group['lr'] for param_group in self.optimizer.param_groups]
+                lr = sum(lrl) / len(lrl)
+
+                if self._master_node:
+                    logging.info(f'Train: {epoch:>3} [{update_idx:>4d}/{updates_per_epoch}]  '
+                                 f'({100. * update_idx / (updates_per_epoch - 1):>3.0f}%)]  '
+                                 f'Loss: {loss.item():#.3g} ({mean_loss:#.3g})  '
+                                 f'LR: {lr:.3e}  ')
+
+            self.scheduler.step_update(num_updates=num_updates, metric=mean_loss)
 
         if hasattr(self.optimizer, 'sync_lookahead'):
             self.optimizer.sync_lookahead()
@@ -120,11 +142,10 @@ class Fit:
         model.eval()
         for i, data in enumerate(self.val_loader):
             loss, prob, target = self.iterate(model, data, self.val_criterion)
-            loss /= self.grad_accumulation
             self._update_metric(loss, prob, target)
 
             _metrics = self._metrics()
-            if self._master_node() and i % self.logging_interval == 0:
+            if self._master_node and (i % self.logging_interval == 0 or i == total - 1):
                 logging.info(self._print(_metrics, epoch, i, total, log_prefix))
 
         return self._metrics()
@@ -141,27 +162,28 @@ class Fit:
             self._update_metric(loss, prob, target)
 
             _metrics = self._metrics()
-            if self._master_node() and i % self.logging_interval == 0:
+            if self._master_node and (i % self.logging_interval == 0 or i == total - 1):
                 logging.info(self._print(_metrics, 0, i, total, 'Test'))
 
         return self._metrics()
 
-    def _backward(self, loss, update_grad):
+    def _backward(self, loss, update_grad, second_order):
         if self.scaler:
-            self._scaler_backward(loss, update_grad)
+            self._scaler_backward(loss, update_grad, second_order)
         else:
-            self._default_backward(loss, update_grad)
+            self._default_backward(loss, update_grad, second_order)
 
         if update_grad:
             self.optimizer.zero_grad()
             if self.model_ema:
                 self.model_ema.update(self.model)
 
-    def _scaler_backward(self, loss, update_grad):
-        self.scaler(loss, self.optimizer, self.model.parameters(), self.clip_grad, update_grad)
+    def _scaler_backward(self, loss, update_grad, second_order):
+        self.scaler(loss, self.optimizer, self.clip_grad, self.clip_mode,
+                    model_parameters(self.model, exclude_head='agc' in self.clip_mode), second_order, update_grad)
 
-    def _default_backward(self, loss, update_grad):
-        loss.backward()
+    def _default_backward(self, loss, update_grad, second_order):
+        loss.backward(second_order)
         if update_grad:
             if self.clip_grad:
                 dispatch_clip_grad(self.model.parameters(), self.clip_grad, mode=self.clip_mode)
@@ -192,9 +214,6 @@ class Fit:
         for k, v in metrics.items():
             log += f'{k}:{v:.6f} | '
         return log[:-3]
-
-    def _master_node(self):
-        return self.local_rank == 0
 
     def init_metrics(self, task, threshold, num_class, num_label, average, top_k=1):
         metric_fn = dict()
