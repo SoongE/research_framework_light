3bc1ed5d1eb69a915bec0045ae570c87bab6a3a1
SoongE <oh1.seungmin@gmail.com>
2023-07-31 16:40:31 +0900

    Add auto_init for models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,5 @@
-from src.models.my_model import *
-from src.models.for_cifar import *
+import glob
+from os.path import dirname, basename, isfile, join
+
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
